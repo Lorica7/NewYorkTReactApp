c3b27bdc26ec1cb8a014270c41f7e6e08e4432b2
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-10-06 11:15:33 -0400

    MERN setup correct

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,28 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Article from "./pages/Article";
 
 
-const App = () => (
+class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
 
   
  <Router>
@@ -15,5 +36,6 @@ const App = () => (
     </div>
     </Router>
 );
-
+ };
+}
 export default App;
